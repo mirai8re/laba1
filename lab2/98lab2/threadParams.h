69ea9881770c2067f472ee8610bb79a2db628e0b
Mironova Anna <100525913+mirai8re@users.noreply.github.com>
2022-10-14 00:28:44 +0300

    lab2

---
+++ lab2/98lab2/threadParams.h
@@ -0,0 +1,27 @@
+//
+// Created by anya on 10/13/2022.
+//
+
+#include <cstdlib>
+
+#ifndef INC_98LAB2_THREADPARAMS_H
+#define INC_98LAB2_THREADPARAMS_H
+
+#endif //INC_98LAB2_THREADPARAMS_H
+
+struct returnValues{
+    returnValues() : min(INT_MIN), max(INT_MAX), average(0.0) {}
+
+    double min;
+    double max;
+    double average;
+};
+
+struct threadParams{
+    threadParams() : arr(NULL), n(-1), ret() {}
+    threadParams(double *arr, int n) : arr(arr), n(n), ret() {}
+
+    double* arr;
+    int n;
+    returnValues ret;
+};
